d5302648a87c59ff8d0ffa56351c00d27127ce40
Desislava Velichkova <76597321+desislavavelichkova@users.noreply.github.com>
2022-12-10 21:58:41 +0200

    Add getFactorial() method

--- Task_3.java
+++ Task_3.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Task_3 {
     public static void main(String[] args) {
-
         // 3.Calculate the sum of 1! + 2! + 3! + ... + n !, n <10.
         //Incoming data: n = 3.
-        //Output data: s = 1.833.
+        //Output data: s = 9.
 
         Scanner s = new Scanner(System.in);
         System.out.print("n = ");
         int n = Integer.parseInt(s.next());
-        int sum = 0;
+        getFactorial(n);
+    }
+    public static void getFactorial(int n){
         int fact = 1;
+        int sum = 0;
         for (int i = 1; i <= n; i++) {
             fact = fact * i;
             sum += fact;
